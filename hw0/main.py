596014a40d8fb78c4309cbd82e59e68762d3d2c6
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-09-16 10:49:44 +0800

    Use Python logger

--- hw0/main.py
+++ hw0/main.py
@@ -2,10 +2,22 @@ import os
 import gzip
 import csv
 import time
+import logging
 import numpy as np
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.svm import SVC
+
+# create the logger
+logger = logging.getLogger('hw0')
+logger.setLevel(logging.DEBUG)
+# log format
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+# create console handler
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+# add the handler to the logger
+logger.addHandler(ch)
 
 def load_mnist_labels(path, kind='train'):
     """ Load MNIST label from `path` """
@@ -32,31 +44,52 @@ def load_mnist(path, kind='train'):
     return images, labels
 
 # load the training data
-print('loading training set')
+logger.info('load training set')
 X_train, y_train = load_mnist('data', kind='train')
+
+def hist_eq(image, n_bins=196):
+    """ Perform histogram equalization on a flattened image array """
+    imhist, bins = np.histogram(image, n_bins, normed=True)
+
+    # cumulative distribution function
+    cdf = imhist.cumsum()
+    # normalize
+    cdf = (n_bins-1) * cdf / cdf[-1]
+
+    # use linear interpolation of cdf to find new values
+    image_eq = np.interp(image, bin[:-1], cdf)
+
+    return image_eq
+
+# apply histogram equalization to images
+X_train = np.apply_along_axis(hist_eq, axis=1, arr=X_train)
 
 # create classifier
 classifier = SVC(C=10, kernel='poly')
 
 # execute
-print('training...')
+logger.info('training START')
 start = time.time()
 classifier.fit(X_train, y_train)
 end = time.time()
-print('elapsed {:.2f}s'.format(end-start))
+logger.info('training END, elapsed {:.2f}s'.format(end-start))
 
 # test the classifier using the original set
 score = classifier.score(X_train, y_train)
-print('score={:.5f}'.format(score))
+logger.debug('score = {:.5f}'.format(score))
 
 # load the test data
-print('loading test set')
+logger.info('load test set')
 X_test = load_mnist_images('data', kind='t10k', n_images=10000)
 # predict
-print('predicting...')
+logger.info('predict START')
+start = time.time()
 y_test = classifier.predict(X_test)
+end = time.time()
+logger.info('predict END, elapsed {:.2f}s'.format(end-start))
 
 # save the result
-print('saving the result')
+filename = 'result.csv'
 y_index = np.arange(10000)
-np.savetxt('result.csv', np.c_[y_index,y_test], fmt='%d', header='id,label', delimiter=',', comments='')
+np.savetxt(filename, np.c_[y_index,y_test], fmt='%d', header='id,label', delimiter=',', comments='')
+logger.info('results are saved as "{}"'.format(filename))

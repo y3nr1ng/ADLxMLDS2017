fac897b5894d0a8cb18d581a16517f3608377cff
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-10-06 19:28:23 +0800

    Ability to load instance-data pair

--- hw1/test.py
+++ hw1/test.py
@@ -1,15 +1,23 @@
 from timit import reader, preprocess
 
-lut = reader.loadMap('data')
-labels = reader.loadLabel('data', 'train')
+samples, lut = reader.loadTIMIT('data', 'train', force=True)
 
+for key, value in samples.items():
+    print(key)
+    print(value)
 
+    break
+
+"""
 for instance, frames in labels.items():
     # remove consecutive frames
     frames = preprocess.trim(frames)
     # convert to character notation
     frames = [lut[label] for label in frames]
     labels[instance] = frames
+
+    break
+"""
 
 #labels[:] = [preprocess.trim(x, lut) for x in labels]
 #print(labels)
--- hw1/timit/reader.py
+++ hw1/timit/reader.py
@@ -1,20 +1,31 @@
-"""
-1) Load the TIMIT dataset.
-2) Remap the phones to english characters (48->48).
-3) Remap the phones (48->39).
-"""
-
+from .frames import Frames
+import pickle
 import os
 import collections
-from .frames import Frames
 
 def parseInstanceID(raw, sep='_'):
+    """
+    Strip the instance ID into three parts:
+    1) Speaker ID
+    2) Sentence ID
+    3) Frame ID
+    where speaker ID and sentence ID will combine into a tuple to use as the key
+    for dictionaries, and frame ID is converted into integer automatically.
+
+    Parameters
+    ----------
+    raw: str
+        the raw instance ID
+    sep: str, default to '_'
+        separator between the IDs
+    """
     speaker, sentence, frame = raw.strip().split(sep)
     return (speaker, sentence), int(frame)
 
-def parseData(raw, sep='\t'):
-    instance, data = raw.strip().partition(sep)
+def parseData(raw, sep=' '):
+    instance, _, data = raw.strip().partition(sep)
     instance, frame = parseInstanceID(instance)
+    data = [float (x) for x in data.split()]
     return instance, frame, data
 
 def loadData(folder, name):
@@ -23,14 +34,19 @@ def loadData(folder, name):
     with open(dataPath, 'r') as fd:
         # use list() as the initializer function for missing keys
         data = collections.defaultdict(Frames)
+        index = 0
         for line in fd:
-            instance, frame, datum = parseData(line)
+            instance, frame, features = parseData(line)
             # raw frame ID starts with 1
             frame -= 1
             # store the label at specified frame position
             frameList = data[instance]
-            frameList[frame] = datum
+            frameList[frame] = features
             data[instance] = frameList
+
+            index += 1
+            print('\rreading entry {}'.format(index), end='')
+        print('\r{} datasets loaded'.format(len(data)))
     return data
 
 def parseLabel(raw, sep=','):
@@ -44,6 +60,7 @@ def loadLabel(folder, name):
     with open(labelPath, 'r') as fd:
         # use list() as the initializer function for missing keys
         labels = collections.defaultdict(Frames)
+        index = 0
         for line in fd:
             instance, frame, label = parseLabel(line)
             # raw frame ID starts with 1
@@ -52,6 +69,10 @@ def loadLabel(folder, name):
             frameList = labels[instance]
             frameList[frame] = label
             labels[instance] = frameList
+
+            index += 1
+            print('\rreading entry {}'.format(index), end='')
+        print('\r{} label-sets loaded'.format(len(labels)))
     return labels
 
 def parsePhoneLUT(raw, sep='\t'):
@@ -79,13 +100,38 @@ def loadMap(folder):
             print('remap \'{}\' as \'{}\''.format(oldPhone, newPhone))
     print()
 
-    # dump the map
+    return phone2char
+
+def mergeLabelAndData(labels, data):
+    samples = {
+        key: list(zip(phones, data[key])) for key, phones in labels.items()
+    }
+    return samples
+
+def loadTIMIT(folder, name, force=False):
+    dataPath = os.path.join(folder, '{}.pkl'.format(name))
+    if os.path.isfile(dataPath) and not force:
+        print('use previously saved pickle')
+        # file exists and allow to use previous result
+        with open(dataPath, 'rb') as fd:
+            samples = pickle.load(fd)
+    else:
+        labels = loadLabel(folder, name)
+        data = loadData(folder, name)
+
+        if len(labels) != len(data):
+            raise ValueError('Unable to form label-data pair')
+        samples = mergeLabelAndData(labels, data)
+
+        # save to pkl
+        with open(dataPath, 'wb') as fd:
+            pickle.dump(samples, fd, pickle.HIGHEST_PROTOCOL)
+
+    # phone-character map
+    lut = loadMap(folder)
     print('phone-character mapping')
-    for key, value in phone2char.items():
+    for key, value in lut.items():
         print(' {} -> {}'.format(key, value))
     print()
 
-    return phone2char
-
-def loadTIMIT(folder, name):
-    pass
+    return samples, lut

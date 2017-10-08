38e1dd9f8bb308c2f47d62402c3b692451046008
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-10-08 12:41:37 +0800

    Try to use Keras LSTM Labels shoud transform to numberic index first.

--- hw1/test.py
+++ hw1/test.py
@@ -1,23 +1,34 @@
 from timit import reader, preprocess
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Embedding, LSTM, Dense
 
-samples, lut = reader.loadTIMIT('data', 'train', force=True)
+testSize = 0.25
+batchSize = 16
 
-for key, value in samples.items():
-    print(key)
-    print(value)
+samples, dimension, lut = reader.loadTIMIT('data', 'train', force=True)
 
-    break
+# determine train set and test set for validations
+instances = [k for k in samples.keys()]
+datasets = train_test_split(instances, test_size=testSize)
+print('Split train and test set, test size is {}'.format(testSize))
+(x_train, y_train) = (datasets[0], [samples[x] for x in datasets[0]])
+(x_test, y_test) = (datasets[1], [samples[x] for x in datasets[1]])
 
-"""
-for instance, frames in labels.items():
-    # remove consecutive frames
-    frames = preprocess.trim(frames)
-    # convert to character notation
-    frames = [lut[label] for label in frames]
-    labels[instance] = frames
+print('Building model...')
+model = Sequential()
+model.add(Embedding(dimension, dimension))
+model.add(LSTM(48, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
-    break
-"""
-
-#labels[:] = [preprocess.trim(x, lut) for x in labels]
-#print(labels)
+print('Training started')
+model.fit(x_train, y_train,
+          batch_size=batchSize, epochs=10,
+          validation_data=(x_test, y_test))
+score, accuracy = model.evaluate(x_test, y_test,
+                            batch_size=batch_size)
+print('Score = ', score)
+print('Accuracy = ', accuracy)
--- hw1/timit/reader.py
+++ hw1/timit/reader.py
@@ -31,12 +31,16 @@ def parseData(raw, sep=' '):
 def loadData(folder, name):
     dataName = '{}.ark'.format(name)
     dataPath = os.path.join(folder, 'mfcc', dataName)
+    dimension = -1
     with open(dataPath, 'r') as fd:
         # use list() as the initializer function for missing keys
         data = collections.defaultdict(Frames)
         index = 0
         for line in fd:
             instance, frame, features = parseData(line)
+            # save feature size
+            if dimension < 0:
+                dimension = len(features)
             # raw frame ID starts with 1
             frame -= 1
             # store the label at specified frame position
@@ -45,16 +49,16 @@ def loadData(folder, name):
             data[instance] = frameList
 
             index += 1
-            print('\rreading entry {}'.format(index), end='')
+            print('\rReading entry {}'.format(index), end='')
         print('\r{} datasets loaded'.format(len(data)))
-    return data
+    return data, dimension
 
 def parseLabel(raw, sep=','):
     instance, label = raw.strip().split(sep)
     instance, frame = parseInstanceID(instance)
     return instance, frame, label
 
-def loadLabel(folder, name):
+def loadLabel(folder, name, lut):
     labelName = '{}.lab'.format(name)
     labelPath = os.path.join(folder, 'label', labelName)
     with open(labelPath, 'r') as fd:
@@ -67,11 +71,11 @@ def loadLabel(folder, name):
             frame -= 1
             # store the label at specified frame position
             frameList = labels[instance]
-            frameList[frame] = label
+            frameList[frame] = lut[label][0]
             labels[instance] = frameList
 
             index += 1
-            print('\rreading entry {}'.format(index), end='')
+            print('\rReading entry {}'.format(index), end='')
         print('\r{} label-sets loaded'.format(len(labels)))
     return labels
 
@@ -80,27 +84,32 @@ def parsePhoneLUT(raw, sep='\t'):
     return p48, p39
 
 def parsePhoneCharPair(raw, sep='\t'):
-    phone, _, char = raw.strip().split(sep)
-    return phone, char
+    phone, index, char = raw.strip().split(sep)
+    return phone, (index, char)
 
 def loadMap(folder):
     # load the phone-character map
     mapPath = os.path.join(folder, '48phone_char.map')
     with open(mapPath, 'r') as fd:
-        phone2char = dict(parsePhoneCharPair(line) for line in fd)
+        lut = dict(parsePhoneCharPair(line) for line in fd)
 
     # simplify the mapping
     mapPath = os.path.join(folder, 'phones', '48_39.map')
     with open(mapPath, 'r') as fd:
         phoneLUT = dict(parsePhoneLUT(line) for line in fd)
-    for oldPhone in phone2char:
+    for oldPhone in lut:
         newPhone = phoneLUT[oldPhone]
         if oldPhone != newPhone:
-            phone2char[oldPhone] = phone2char[newPhone]
-            print('remap \'{}\' as \'{}\''.format(oldPhone, newPhone))
+            oldMap = lut[oldPhone]
+            newMap = lut[newPhone]
+            print('Remap {}\'{}\' as {}\'{}\''.format(
+                    oldPhone, oldMap, newPhone, newMap))
+            newMap = (newMap[0], oldMap[1])
+            lut[oldPhone] = newMap
+
     print()
 
-    return phone2char
+    return lut
 
 def mergeLabelAndData(labels, data):
     samples = {
@@ -109,15 +118,23 @@ def mergeLabelAndData(labels, data):
     return samples
 
 def loadTIMIT(folder, name, force=False):
+    # phone-character map
+    lut = loadMap(folder)
+    print('Phone-character mapping')
+    for key, value in lut.items():
+        print(' {} -> {}'.format(key, value))
+    print()
+
     dataPath = os.path.join(folder, '{}.pkl'.format(name))
     if os.path.isfile(dataPath) and not force:
-        print('use previously saved pickle')
+        print('Use previously saved pickle')
         # file exists and allow to use previous result
         with open(dataPath, 'rb') as fd:
             samples = pickle.load(fd)
+            dimension = pickle.load(fd)
     else:
-        labels = loadLabel(folder, name)
-        data = loadData(folder, name)
+        labels = loadLabel(folder, name, lut)
+        data, dimension = loadData(folder, name)
 
         if len(labels) != len(data):
             raise ValueError('Unable to form label-data pair')
@@ -126,12 +143,6 @@ def loadTIMIT(folder, name, force=False):
         # save to pkl
         with open(dataPath, 'wb') as fd:
             pickle.dump(samples, fd, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(dimension, fd, pickle.HIGHEST_PROTOCOL)
 
-    # phone-character map
-    lut = loadMap(folder)
-    print('phone-character mapping')
-    for key, value in lut.items():
-        print(' {} -> {}'.format(key, value))
-    print()
-
-    return samples, lut
+    return samples, dimension, lut

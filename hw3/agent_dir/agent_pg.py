Fix gradient policy

hw3/agent_dir/agent_pg.py
@@ -9,6 +9,9 @@ from keras.layers import Reshape, Conv2D, Flatten, Dense
 from keras.optimizers import RMSprop
 from keras.utils import print_summary, to_categorical
 
+#from keras import backend as K
+#K.set_image_dim_ordering('th')
+
 from pprint import pprint
 np.set_printoptions(threshold=np.inf)
 
@@ -17,12 +20,17 @@ class Agent_PG(Agent):
     History = namedtuple('History',
                          ['state', 'probability', 'gradient', 'reward'])
 
+    FRAME_WIDTH = 80
+    FRAME_HEIGHT = 80
+
     def __init__(self, env, args):
         """
         Initialize every things you need here.
         For example: building your model
         """
         super(Agent_PG,self).__init__(env)
+
+        self._num_actions = self.env.get_action_space().n
 
         self._build_network()
         if args.test_pg or args.reuse:
@@ -36,12 +44,14 @@ class Agent_PG(Agent):
         """
         Create a base network.
         """
+        size = (Agent_PG.FRAME_WIDTH, Agent_PG.FRAME_HEIGHT, 1)
         model = Sequential([
-            Conv2D(16, (8, 8), activation='relu', strides=(4, 4), input_shape=(STATE_LENGTH, FRAME_WIDTH, FRAME_HEIGHT)),
+            Conv2D(16, (8, 8), activation='relu', strides=(4, 4),
+                   input_shape=size, data_format='channels_last'),
             Conv2D(32, (4, 4), activation='relu', strides=(2, 2)),
             Flatten(),
             Dense(128, activation='relu'),
-            Dense(self.env.get_action_space().n, activation='softmax')
+            Dense(self._num_actions, activation='softmax')
         ])
         print_summary(model)
         self.model = model
@@ -57,19 +67,19 @@ class Agent_PG(Agent):
         """
         pass
 
-    def train(self):
+    def train(self, num_ep=100, save_interval=500):
         """
         Implement your training algorithm here
         """
         avg_score = None
-        for i_ep in range(10000):
+        for i_ep in range(num_ep):
             score, loss = self._train_once()
             if avg_score is None:
                 avg_score = score
             else:
                 avg_score = avg_score*0.99 + score*0.01
             print('ep {}, score(avg) = {}({:.06f}), loss = {:.06f}'.format(i_ep, score, avg_score, loss))
-            if i_ep % 100 == 0:
+            if i_ep % save_interval == 0:
                 self.model.save_weights(Agent_PG.WEIGHT_FILE)
                 print('...saved')
         self.model.save_weights(Agent_PG.WEIGHT_FILE)
@@ -78,56 +88,47 @@ class Agent_PG(Agent):
         """
         Train a single episode.
         """
-        # initialize
-        observation = self.env.reset()
         score = 0
         history = []
+        prev_state = None
 
-        # run a single episode
         done = False
-        prev_field = None
-        t = 0
+        observation = self.env.reset()
         while not done:
-            # execute a step
-            action, p_action = self.make_action(observation, test=False)
-            #pprint('{} {}'.format(['{0:.3f}'.format(i) for i in p_action], action))
-            observation, reward, done, _ = self.env.step(action)
+            curr_state = Agent_PG._preprocess(observation)
+            if prev_state is not None:
+                state = curr_state - prev_state
+            else:
+                state = np.zeros_like(curr_state)
+            prev_state = curr_state
 
+            action, predict, p_action = self.make_action(state, test=False)
+            observation, reward, done, _ = self.env.step(action)
             score += reward
 
-            curr_field, player = Agent_PG._preprocess(observation)
-            if prev_field is None:
-                prev_field = np.zeros_like(curr_field)
-            # concat all the variables
-            state = np.hstack([player, curr_field, prev_field])
-            prev_field = curr_field
-
-            # calculate probability gradient
-            p_decision = to_categorical(action,
-                                        num_classes=self.env.get_action_space().n)
+            # decision gradient
+            p_decision = to_categorical(action, num_classes=self._num_actions)
             gradient = p_decision.astype(np.float32) - p_action
 
-            # remember the result
-            entry = Agent_PG.History(state, p_action, gradient, reward)
+            entry = Agent_PG.History(state, predict, gradient, reward)
             history.append(entry)
 
-        # extract the results
         states, probability, gradients, rewards = zip(*history)
+        probability = np.vstack(probability)
+        gradients = np.vstack(gradients)
+        rewards = np.vstack(rewards)
 
         # discount rewards
-        rewards = np.vstack(rewards)
         rewards = self._discount_rewards(rewards, gamma=gamma)
         # normalize
         rewards -= np.mean(rewards)
         rewards /= np.std(rewards)
 
         # attenuate the gradients
-        gradients = np.vstack(gradients)
         gradients *= rewards
 
         # batch training
-        X = np.vstack(states)
-        probability = np.vstack(probability)
+        X = np.vstack([states])
         Y = probability + lr * gradients
         loss = self.model.train_on_batch(X, Y)
 
@@ -143,63 +144,32 @@ class Agent_PG(Agent):
             d_rewards[i] = running_add
         return d_rewards
 
-    def make_action(self, observation, test=True):
+    def make_action(self, state, test=True):
         """
         Return predicted action of your agent
-
-        Input:
-            observation: np.array
-                current RGB screen of game, shape: (210, 160, 3)
-
-        Return:
-            action: int
-                the predicted action from trained model
         """
-        curr_field, player = Agent_PG._preprocess(observation)
-        if self._prev_field is None:
-            self._prev_field = np.zeros_like(curr_field)
-        # concat all the variables
-        state = np.hstack([player, curr_field, self._prev_field])
-        self._prev_field = curr_field
-
         state = np.expand_dims(state, axis=0)
-        p_action = self.model.predict(state, batch_size=1).flatten()
+        predict = self.model.predict(state, batch_size=1).flatten()
         # normalize the PDF
-        p_action /= np.sum(p_action)
+        p_action = predict / np.sum(predict)
 
         # determine the action according to the PDF
-        action = np.random.choice(self.env.get_action_space().n, p=p_action)
+        action = np.random.choice(self._num_actions, p=p_action)
         if test:
             return action
         else:
-            return action, p_action
+            return action, predict, p_action
 
     @staticmethod
-    def _preprocess(observation):
+    def _preprocess(observation, size=(80, 80)):
         """
-        Process (split fields and segmented) the observation field.
-
-        Input:
-            observation: np.array
-                current RGB screen of game, shape: (210, 160, 3)
-
-        Return:
-            observation: np.array
-                processed binary image, only play field and our board
+        Preprocess the observation.
+        1) Convert to grayscale
+        2) Resize
         """
-        # convert red layer binarized float
-        observation = observation[..., 2].astype(np.float32)
-        observation[observation < 50] = 0.
-        observation[observation >= 50] = 1.
+        observation = np.dot(observation[..., :3], [0.2126, 0.7152, 0.0722])
+        observation = observation.astype(np.uint8)
 
-        # field
-        #   x=20, y=34, w=120, h=160
-        field = observation[34:194:2, 20:140:2]
-        player = observation[34:194:2, 140]
+        observation = cv2.resize(observation, size)
 
-        # apply the weight
-        weights = np.arange(1, 61, dtype=np.float32) / 60
-        field *= weights
-        field = np.sum(field, axis=1)
-
-        return field, player
+        return np.expand_dims(observation.astype(np.float32), axis=2)
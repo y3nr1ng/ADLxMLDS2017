dce2e17b3e3b08f3a74bb1f59354af343052d6e1
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-01-06 15:15:39 +0800

    Add test text parser

--- hw4/generate.py
+++ hw4/generate.py
@@ -9,18 +9,29 @@ handler.setFormatter(formatter)
 logging.basicConfig(level=logging.DEBUG, handlers=[handler])
 logger = logging.getLogger(__name__)
 
+import pandas as pd
+
 import data, model
 from gan import WassersteinGAN
+import utils
+
+def _test_text_splitter(string, span=2):
+    '''Split every nth space.'''
+    tags = string.split()
+    return [' '.join(tags[i:i+span]) for i in range(0, len(tags), span)]
+
+def load_labels(label_path):
+    df = pd.read_csv(label_path, index_col=0, names=['id', 'tags'])
+    return utils.text_to_onehot(df, splitter=_test_text_splitter)
 
 def parse_arguments():
     parser = argparse.ArgumentParser('HW4')
-    parser.add_argument('label', type=str, help='label source')
+    parser.add_argument('label_path', type=str, help='label source')
     parser.add_argument('--gpus', type=str, default='0')
     parser.add_argument(
-        '-n',
-        type=int, default=5,
-        help='number of images per label'
+        '-n', type=int, default=5, help='number of images per label'
     )
+    parser.add_argument('--seed', type=int, default=42)
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -28,11 +39,15 @@ if __name__ == '__main__':
 
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
 
-    #TODO modify here
     data_sampler = data.DataSampler()
-    noise_sampler = data.NoiseSampler()
+    noise_sampler = data.NoiseSampler(seed=args.seed)
     g_net = model.Generator()
     d_net = model.Discriminator()
 
-    wgan = WassersteinGAN(g_net, d_net, data_sampler, noise_sampler)
-    wgan.train(epochs=args.epochs, batch_size=args.batch_size)
+    #wgan = WassersteinGAN(g_net, d_net, data_sampler, noise_sampler)
+    #wgan.restore()
+
+    labels = load_labels(args.label_path)
+    for i in range(labels.shape[0]):
+        logger.info('generate images for \'{}\''.format(labels[i, ...]))
+        #wgan.generate()
